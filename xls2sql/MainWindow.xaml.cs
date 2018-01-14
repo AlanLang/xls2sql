efa736433cec58a7c346cf27c8dbeaf36e71d532
alan <langwdalan@gmail.com>
2018-01-14 19:41:58 +0800

    初始化项目

--- xls2sql/MainWindow.xaml.cs
+++ xls2sql/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,7 @@ namespace xls2sql
     {
         public MainWindow()
         {
+            OleDbConnection conn = new OleDbConnection("");
             InitializeComponent();
         }
     }
