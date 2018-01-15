4976767c19517dce07a8c1a9dc1a04a1da9d0c3f
AlanLang <langwdalan@gmail.com>
2018-01-15 10:03:23 +0800

    更新

--- xls2sql/MainWindow.xaml.cs
+++ xls2sql/MainWindow.xaml.cs
@@ -26,5 +26,43 @@ namespace xls2sql
             OleDbConnection conn = new OleDbConnection("");
             InitializeComponent();
         }
+
+
+        private void Msg(string msg)
+        {
+            messagelog.AppendText(msg + "\n");
+            msg += "  [" + DateTime.Now.ToString("yyyyMMddHHmmss") + "]";
+            string logPath = AppDomain.CurrentDomain.BaseDirectory + "Log/";
+            if (!System.IO.Directory.Exists(logPath))
+                System.IO.Directory.CreateDirectory(logPath);
+            string logFile = logPath + "Log-" + DateTime.Now.ToString("yyyy-MM-dd") + ".log";
+            System.IO.File.AppendAllLines(logFile, new string[] { msg });
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void ImpFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void closeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Msg("程序退出");
+            this.Close();
+        }
+
+        private void TextBox_PreviewDragOver(object sender, DragEventArgs e)
+        {
+
+        }
     }
 }
